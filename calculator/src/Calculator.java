9f465c1fe39a7f3fbd88b3245a01c36c1ddd4c39
Dan Rosen <dan@marakana.com>
2012-08-27 16:55:40 -0400

    added some pseudocode

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,8 +1,29 @@
+import java.util.Stack;
 
 public class Calculator {
 
 	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("Usage: Calculator <expression>");
+			System.exit(1);
+		}
 
+		// initialize an empty stack
+		Stack<Integer> stack = new Stack<Integer>();
+
+		// split expression string into tokens
+		String expression = args[0];
+		String[] tokens = expression.split(" ");
+
+		// for each token ...
+		for (String token : tokens) {
+			//   - if number, push onto stack
+			//   - if operator:
+			//       pop two numbers off stack,
+			//       compute result,
+			//       push result onto stack
+		}
+		// result is on the stack
 	}
 
 }
